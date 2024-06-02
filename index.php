96a1a768f088d333c39436a02de347db88fdcc34
Kaveh Tafakori <Tafakori.K@morsa.com>
2024-06-02 16:49:08 +0330

    change in save file

--- index.php
+++ index.php
@@ -4,19 +4,20 @@ require 'CommandHandler.php';
 require 'TelegramBot.php';
 
 $dbPath = 'commands.db';
+$bot = new TelegramBot();
+$commandHandler = new CommandHandler($dbPath);
+$image = new CmdImage();
+$image->createImageObject(1080, 1080);
 
 // Usage example
-$bot = new TelegramBot();
 // Create an instance of the CommandHandler class
-$commandHandler = new CommandHandler($dbPath);
-$command = $commandHandler->readAndUpdateCommand();
+while(true) {
+    $command = $commandHandler->readAndUpdateCommand();
 $text = $command['example'];
 $caption = "command: {$command['command']} in linux,
 when use it? {$command['case_of_use']}.  
 {$command['description']}
 which options?  {$command['options']}";
-$image = new CmdImage();
-$image->createImageObject(1080, 1080);
 $image->createBackgroundRandomLight();
 $image->addBackground('./linux.jpg', 50);
 $image->addText($text, 60, './times new roman.ttf', [0, 200, 0], [0, 0, 0, 63], 20);
@@ -24,4 +25,6 @@ $image->addSticker('./sticker.png');
 $img = $image->returnImage('jpeg', './image.jpg');
 
 $bot->send($caption, './image.jpg');
+sleep(rand(1, 10));
+}
 ?>
